bb9eae7799da44dbd215c9b588bba48599aa03cf
joseph <joseph@cleversteam.com>
2019-01-23 14:58:20 +0000

    refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -33,7 +33,7 @@ class PortfoliosController < ApplicationController
   	@portfolio_item = Portfolio.find(params[:id])
    
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The Record was successfully updated.' }
       else
         format.html { render :edit }
@@ -49,11 +49,17 @@ class PortfoliosController < ApplicationController
   	#performs the look up
   	@portfolio_item = Portfolio.find(params[:id])
     #this destroys the record
-    # dont think this is needed anymore
-    #@portfolio_item.destroy
+    #used has_many :technologies, dependent: :destroy in portfolio rb to fix error
+    @portfolio_item.destroy
     
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'The Record was successfully deleted.' }
     end
+  end
+
+  private
+  
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
