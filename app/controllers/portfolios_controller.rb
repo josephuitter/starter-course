c9aea8a59d6d6eaf5e5c72ecb8e45641fa0372f1
joseph <joseph@cleversteam.com>
2019-01-24 14:43:44 +0000

    refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
 	
   def index
@@ -10,13 +11,10 @@ class PortfoliosController < ApplicationController
   end
 
 	def new
-		@portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build}
 	end
 
 	def create
-    @portfolio_item = Portfolio.new(portfolio_params)
-
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
@@ -27,12 +25,9 @@ class PortfoliosController < ApplicationController
   end
   
   def edit
-  	@portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-  	@portfolio_item = Portfolio.find(params[:id])
-   
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The Record was successfully updated.' }
@@ -43,12 +38,10 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-  	@portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
   	#performs the look up
-  	@portfolio_item = Portfolio.find(params[:id])
     #this destroys the record
     #used has_many :technologies, dependent: :destroy in portfolio rb to fix error
     @portfolio_item.destroy
@@ -63,4 +56,9 @@ class PortfoliosController < ApplicationController
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
+  
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
 end
