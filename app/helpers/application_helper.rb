c9ee267077df43bfe91ee4dab52327b14a27aa98
joseph <joseph@cleversteam.com>
2019-01-29 15:18:42 +0000

    implimented refactor for navigation and design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,48 @@ module ApplicationHelper
 	 	end
 	end
 
-	  
+  def nav_items
+	[
+		{
+			url: root_path,
+			title: 'home'
+		},
+		{
+			url: about_path,
+			title: 'about'
+		},
+		{
+			url: contact_path,
+			title: 'contact'
+		},
+		{
+			url: blogs_path,
+			title: 'blog'
+		},
+		{
+			url: portfolios_path,
+			title: 'portfolio'
+		}								
+  	]
+  end	
+
+
+  def nav_helper style, tag_type
+  	nav_links = ""
+  	nav_items.each do |item|
+  	nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+  end	
+
+
+
+		nav_links.html_safe
+
+  end
+
+  def active? path
+  	"active" if current_page? path
+  	
+  end
   
   def copyright_generator
   	JosephViewTool::Renderer.copyright 'Joseph Uittermark', 'All Rights Reserved'
