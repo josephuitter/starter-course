16542295543541027d4e1b53bdb69c730a436d39
joseph <joseph@cleversteam.com>
2019-01-29 13:48:01 +0000

    updated  styles for pottfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,10 +12,13 @@ class PortfoliosController < ApplicationController
   end
 
 	def new
+    @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build}
 	end
 
 	def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
