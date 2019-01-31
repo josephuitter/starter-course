2d9775d7b8d3334295c098a9841fb66bc846bb83
joseph <joseph@cleversteam.com>
2019-01-31 15:47:37 +0000

    added redis libary and created comment generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-	validates_presence_of :name 
-	def first_name
+	validates_presence_of :name
+
+  has_many :comments, dependent: :destroy
+	
+  def first_name
 		self.name.split.first
 	end
 
