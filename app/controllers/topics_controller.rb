7363b95cfac0869e45ae33d5d4ca237919df453a
joseph <joseph@cleversteam.com>
2019-02-01 14:38:53 +0000

    implimented blog widget and updated topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,15 +1,24 @@
 class TopicsController < ApplicationController
-	layout 'blog'
+  before_action :set_sidebar_topics
+  layout 'blog'
+
   def index
-  	@topics = Topic.all
+    @topics = Topic.all
   end
 
   def show
-  	@topic = Topic.find(params[:id])
-  	if logged_in?(:site_admin)
-      @blogs = @topic.blogs.recent.page(params[:page]).per (5)
+    @topic = Topic.find(params[:id])
+
+    if logged_in?(:site_admin)
+      @blogs = @topic.blogs.recent.page(params[:page]).per(5)
     else
-      @blogs = @topic.blogs.published.page(params[:page]).per (5)
-    end    
+      @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
+    end
+  end
+
+  private
+
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
   end
 end
