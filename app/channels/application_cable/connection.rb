7d380f4b3cb85519c79138b07cf7aeab7bc84c8e
joseph <joseph@cleversteam.com>
2019-02-01 10:21:51 +0000

    fixed comments not being published

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,31 +1,29 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-  	identified_by :current_uder
+    identified_by :current_user
 
-  	def guest_user
-  		guest = Guest_User.new
-  		guest.id = guest.object_id
-  		guest.name = "Guest User"
-  		guest.first_name = "Guest"
-  		guest.last_name = 'User'
-  		guest.email = 'guest@user.com'
-  		guest
-  	end
+    def guest_user
+      guest = GuestUser.new
+      guest.id = guest.object_id
+      guest.name = "Guest User"
+      guest.first_name = "Guest"
+      guest.last_name = "User"
+      guest.email = "guest@user.com"
+      guest
+    end
 
-  	def connect
-  		self.current_user = find_verified_user || guest_user
-  		logger.add_tags 'ActionCable', current_user.email
-  		logger.add_tags 'ActionCable', current_user.id
-  	end
+    def connect
+      self.current_user = find_verified_user || guest_user
+      logger.add_tags 'ActionCable', current_user.email
+      logger.add_tags 'ActionCable', current_user.id
+    end
 
-  	protected
+    protected
 
-  	def find_verified_user
-  		if find_verified_user = env['warden'].user
-  			verified_user
-  			
-  		end
-  		
-  	end
+    def find_verified_user
+      if verified_user = env['warden'].user
+        verified_user
+      end
+    end
   end
 end
--- app/channels/blogs_channel.rb
+++ app/channels/blogs_channel.rb
@@ -1,13 +1,13 @@
-class BlogsChannel < ApplicationCable::BlogsChannel
+class BlogsChannel < ApplicationCable::Channel
 	def subscribed
-		stream_from "blogs_#{params['blog_id']}_channel"
+	  stream_from "blogs_#{params['blog_id']}_channel"
 	end
 
 	def unsubscribe
 	end
 
 	def send_comment(data)
-		current_user.comments.create!(content: data['content'], blog_id: data['blog_id'])
+	  current_user.comments.create!(content: data['comment'], blog_id: data['blog_id'])
 	end
 		
 end
